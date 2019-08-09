de7c291636c523ef184c5e9d1a0dd64ff657d5f9
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-09 08:40:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ end
 def summon_captain_planet(planeteer_calls)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
-planeteer_calls.map{|memo| memo.capitalize (#"!")}
+planeteer_calls.map{|memo| memo.capitalize"#{!}"}
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
